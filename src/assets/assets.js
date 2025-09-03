4c16a1c7d4c45d9d5c5fff76d76fb01fdc180f56
priyank1 <rautelapriyank5@gmail.com>
2025-09-03 12:14:44 +0530

    Added login form with validation

---
+++ src/assets/assets.js
@@ -0,0 +1,18 @@
+import hero_img from 'hero-img.png';
+import app_icon from 'app-icon.webp';
+import footer_img from 'footer-img.webp';
+import expand from 'expand.webp';
+import explore from 'Expore.webp'
+import future from 'Future.png';
+import Partner from 'Partner.webp';
+import neon_glow from 'neon-glow.webp';
+export default {
+    hero_img,
+    app_icon,
+    footer_img,
+    expand,
+    future,
+    explore,
+    Partner,
+    neon_glow
+};
